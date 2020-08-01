950cb3b47f7ce8385a52cc1eb0d99b6180fbd432
Chanjung Kim <freiyer.paxbun@gmail.com>
2020-08-02 00:33:56 +0900

    Add build and bindings generation

---
+++ build.rs
@@ -0,0 +1,54 @@
+use std::fs::File;
+use std::io::{Result, Write};
+use std::path::Path;
+
+fn run_cmake(source_dir: &str) {
+    let sources = [
+        "source/dy.cc",
+        "public/dy.h",
+        "private/dy.p.hh",
+        "CMakeLists.txt",
+    ];
+    for src in sources.iter() {
+        println!("cargo:rustc-if-changed={}/{}", source_dir, src);
+    }
+
+    let install_dir = cmake::build(source_dir);
+    println!(
+        "cargo:rustc-link-search=native={}/bin",
+        install_dir.display()
+    );
+    println!("cargo:rustc-link-lib=static={}", source_dir);
+    println!("cargo:rustc-link-lib=dylib={}", source_dir);
+}
+
+fn generate_bindings(header_path: &str) -> Result<()> {
+    // generate bindings
+    let bindings = bindgen::Builder::default()
+        .header(header_path)
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate_comments(false)
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out = Path::new("src/bindings.rs");
+    let mut file = File::create(&out)?;
+
+    // write to file
+    let error_types = [
+        "non_camel_case_types",
+        "non_snake_case",
+        "non_upper_case_globals",
+        "dead_code",
+    ];
+    for error_type in error_types.iter() {
+        writeln!(file, "#![allow({})]", error_type)?;
+    }
+    write!(file, "\n{}", bindings.to_string())?;
+    Ok(())
+}
+
+fn main() -> Result<()> {
+    run_cmake("dy");
+    generate_bindings("dy/public/dy.h")
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
+mod bindings;
+use bindings::*;
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        unsafe {
+            let dy = dy_make_i(15);
+            assert_eq!(dy_get_type(dy), _dy_type_t_dy_type_i);
+            assert_eq!(dy_get_i(dy), 15);
+        }
     }
 }
